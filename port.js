fddb46db95707a6561a70df65974617cb5c4376c
dorian921 <144111488+dorian921@users.noreply.github.com>
2024-06-04 13:54:37 +0200

    kip

--- port.js
+++ port.js
@@ -12,13 +12,16 @@ var skillsbtn = document.getElementById("skillsbtn")
     var info2 = document.getElementById('port-info2id');
     var info3 = document.getElementById('port-info3id');
     var info4 = document.getElementById('port-info4id'); 
+    var info5 = document.getElementById('port-info5id');
     if (info1.style.display === "none" || info1.style.display === "") {
         info1.style.display = "block";
         info2.style.display = "none";
         info3.style.display = "none"; 
         info4.style.display = "none"; 
+        info5.style.display = "none";
     } else {
         info1.style.display = "none";
+        
     }
 
     
@@ -28,14 +31,16 @@ document.getElementById('overbtn').onclick = function() {
   var info2 = document.getElementById('port-info2id'); 
   var info3 = document.getElementById('port-info3id');
   var info4 = document.getElementById('port-info4id'); 
-  
+  var info5 = document.getElementById('port-info5id');
   if (info2.style.display === "none" || info2.style.display === "") {
       info2.style.display = "block";
       info1.style.display = "none"; 
       info3.style.display = "none"; 
       info4.style.display = "none"; 
+      info5.style.display = "none";
   } else {
       info2.style.display = "none"; 
+      
   }
 }
 
@@ -44,13 +49,17 @@ document.getElementById('probtn').onclick = function() {
   var info2 = document.getElementById('port-info2id');
   var info3 = document.getElementById('port-info3id'); 
   var info4 = document.getElementById('port-info4id');
+  var info5 = document.getElementById('port-info5id');
   if (info3.style.display === "none" || info3.style.display === "") {
       info3.style.display = "block";
       info2.style.display = "none"; 
       info1.style.display = "none";
       info4.style.display = "none";
+      info5.style.display = "none";
   } else {
       info3.style.display = "none"; 
+      
+      
   }
 }
 
@@ -59,14 +68,19 @@ document.getElementById('skillsbtn').onclick = function() {
   var info2 = document.getElementById('port-info2id');
   var info3 = document.getElementById('port-info3id');
   var info4 = document.getElementById('port-info4id'); 
+  var info5 = document.getElementById('port-info5id');
   
-  if (info4.style.display === "none" || info4.style.display === "") {
+  if (info4.style.display === "none" || info4.style.display === "" )  {
       info4.style.display = "block";
+      info5.style.display = "block";
+
       info3.style.display = "none"; 
       info1.style.display = "none";
       info2.style.display = "none";
   } else {
       info4.style.display = "none"; 
+      info5.style.display = "none";
+      
   }
 }
 
